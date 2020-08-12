8640373c6db2e73aaaeb2a7e98df9fa6cae1ba18
sithiphone <sithiphone@fe-nuol.edu.la>
2020-08-12 14:56:16 +0700

    separate class

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Project 1'),
-      centerTitle: true,
-      backgroundColor: Colors.red[500],
-    ),
-    body: Center(
-      child: Text('Hello, MOIC!', style: TextStyle(
-          fontSize: 50.0, color: Colors.deepOrange,
-          fontFamily: 'IndieFlower',
-      ),),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('Click'),
-      backgroundColor: Colors.red[500],
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Project 1'),
+          centerTitle: true,
+          backgroundColor: Colors.red[500],
+        ),
+        body: Center(
+          child: Text('Hello, MOIC!', style: TextStyle(
+            fontSize: 50.0, color: Colors.deepOrange,
+            fontFamily: 'IndieFlower',
+          ),),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('Click'),
+          backgroundColor: Colors.red[500],
+        ),
+      ),
+    );
+  }
+}
