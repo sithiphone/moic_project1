0acf4327d9604f96c7bd3f79558b9e0aa6e66140
sithiphone <sithiphone@fe-nuol.edu.la>
2020-08-13 09:14:09 +0700

    Icon

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,10 @@ class Home extends StatelessWidget{
           backgroundColor: Colors.red[500],
         ),
         body: Center(
-          child: Image(
-            image: AssetImage('assets/laos2.jpg')
+          child: Icon(
+            Icons.access_alarms,
+            size: 80.0,
+            color: Colors.red,
           ),
         ),
         floatingActionButton: FloatingActionButton(
